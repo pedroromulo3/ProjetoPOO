fa8803b3a36104e59f87b4aec0c1e74ba1bede39
Pedro-Carvalhodev <pedocarvalho415@gmail.com>
2025-07-02 00:37:11 -0300

    Inicio do Acervo.(quase nada pq to com sono dms)

--- core.py
+++ core.py
@@ -2,3 +2,11 @@ from models import *
 class Acervo:
     def __init__(self):
         self.obras = {}
+
+    def __iadd__(self, obra: Obra):
+        self.remover(obra)
+        return self
+    
+    def __isub__(self, obra: Obra):
+        self.remover(obra)
+        return self
